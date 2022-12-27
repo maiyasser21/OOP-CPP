f85a532f5f984d79fe65da97756de51c19448fb2
Mai Yasser <maiymuhammad8@gmail.com>
2022-12-28 01:02:44 +0200

    lab7 done

--- geoshape-day7/main.cpp
+++ geoshape-day7/main.cpp
@@ -43,7 +43,7 @@ public:
 };
 class Triangle : public GeoShape
 {
-    public:
+public:
     Triangle(float x, float y) : GeoShape (x,y)
     {
 
@@ -55,7 +55,11 @@ class Triangle : public GeoShape
 };
 class Rect : public GeoShape
 {
-    public:
+public:
+    Rect()
+    {
+
+    }
     Rect(float x, float y) : GeoShape (x,y)
     {
 
@@ -67,7 +71,15 @@ class Rect : public GeoShape
 };
 class Circle : private GeoShape
 {
-    public:
+public:
+    Circle()
+    {
+        cout<<"this is the circle constructor"<<endl;
+    }
+    ~Circle()
+    {
+        cout<<"this is the distructor"<<endl;
+    }
     Circle(float x) : GeoShape (x)
     {
 
@@ -85,15 +97,49 @@ class Circle : private GeoShape
         return 22.0/7*d1*d1;
     }
 };
+class Square : private Rect
+{
+public:
+    Square()
+    {
+        cout<<"empty constructor"<<endl;
+    }
+    Square(float x) : Rect(x,x)
+    {
+
+    }
+    void setSQDim (float x)
+    {
+        d1=d2=x;
+    }
+    float getSQDim ()
+    {
+        return d1;
+    }
+    float calculateArea()
+    {
+        return Rect::calculateArea();
+    }
+};
 
 
 
 int main()
 {
-/*Triangle t1(2,3);
-cout<<"area is : "<<t1.calculateArea()<<endl;
-Rect r1(3,3);
-cout<<"area is : "<<r1.calculateArea()<<endl;*/
+//Triangle t1(2,3);
+//cout<<"area is : "<<t1.calculateArea()<<endl;
+//Rect r1(3,3);
+//cout<<"area is : "<<r1.calculateArea()<<endl;*/
+//Circle c1(4);
+//cout<<"circle area is "<<c1.calculateArea()<<endl;
+//cout<<"violation"<<c1.setD2(3)<<endl; // error inaccessible within this context
+    //Square sq1;
+//cout<<"violation"<<sq1.setD1(3)<<endl; // error inaccessible within this context
+    //sq1.setSQDim(4);
+    //cout<<"square area is : "<<sq1.calculateArea()<<endl;
+    Circle c1;
+    c1.setRadius(3);
+    cout<<"circle area is "<<c1.calculateArea()<<endl;
     return 0;
 
 }
